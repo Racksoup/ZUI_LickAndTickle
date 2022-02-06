6620de3919466143f2f6816c64bcdf54e65eb541
Racksoup <rack.connor1@gmail.com>
2022-02-05 21:36:22 -0500

    updated names to work with other addons

--- core.lua
+++ core.lua
@@ -1,12 +1,12 @@
 ZUI_LickAndTickle = LibStub("AceAddon-3.0"):NewAddon("ZUI_LickAndTickle", "AceConsole-3.0", "AceEvent-3.0")
 local L = LibStub("AceLocale-3.0"):GetLocale("ZUI_LickAndTickle_Locale")
-local ZUI_GUI = LibStub("AceGUI-3.0")
+local LAT_GUI = LibStub("AceGUI-3.0")
 local ZUI_LDB = LibStub("LibDataBroker-1.1"):NewDataObject("ZUI_LickAndTickle", {
     type = "data source",
     text = L["ZUI Lick and Tickle"],
     icon = GetItemIcon(5041),
     OnClick = function()
-        if (ZUI_GUI.frame:IsVisible())
+        if (LAT_GUI.frame:IsVisible())
         then
             ZUI_LickAndTickle:OnDisable();
         else
@@ -50,21 +50,21 @@ function ZUI_LickAndTickle:OnEnable()
 end
 
 function ZUI_LickAndTickle:OnDisable()
-    ZUI_GUI.frame:Hide()
+    LAT_GUI.frame:Hide()
 end
 
 function ZUI_LickAndTickle:CreateBtns(frameName, parent, btnText, emote)
     local points = {}
     if (btnText == "Tickle") then points[1] = 100 points[2] = -100 else points[1] = 0 points[2] = -40  end
-    ZUI_GUI.frame = CreateFrame("Button", frameName, parent)
-    frame = ZUI_GUI.frame
-    frame:SetFrameStrata("HIGH")
-    frame:SetFrameLevel(0)
-    frame:SetSize(64, 20)
-    frame:SetPoint("TOPLEFT", points[1], points[2])
-    frame:SetScript("OnClick", function() ZUI_LickAndTickle:btnClicked(emote) end)
+    LAT_GUI.frame = CreateFrame("Button", frameName, parent)
     
-    text = frame:CreateFontString("text", "HIGH")
+    LAT_GUI.frame:SetFrameStrata("HIGH")
+    LAT_GUI.frame:SetFrameLevel(0)
+    LAT_GUI.frame:SetSize(64, 20)
+    LAT_GUI.frame:SetPoint("TOPLEFT", points[1], points[2])
+    LAT_GUI.frame:SetScript("OnClick", function() ZUI_LickAndTickle:btnClicked(emote) end)
+    
+    text = LAT_GUI.frame:CreateFontString("text", "HIGH")
     text:SetPoint("CENTER")
     text:SetFont("Fonts\\FRIZQT__.TTF", 20, "THINOUTLINE")
     text:SetText(btnText)  
