2903ff4d6e893ad395d9520e3479734e0e37a097
Racksoup <rack.connor1@gmail.com>
2022-02-05 19:00:45 -0500

    tracks if target has been licked or tickled in DB

--- core.lua
+++ core.lua
@@ -33,12 +33,15 @@ local defaults = {
         minimap = {
             hide = false,
         },
+        licked = {},
+        tickled = {},
     },
 }
 
 function ZUI_LickAndTickle:OnInitialize()
     self.db = LibStub("AceDB-3.0"):New("ZUI_LickAndTickleDB", defaults, true)
     icon:Register("ZUI_LickAndTickle", ZUI_LDB, self.db.profile.minimap)
+    self.db:ResetDB()
 end
 
 function ZUI_LickAndTickle:OnEnable()
@@ -52,14 +55,14 @@ end
 
 function ZUI_LickAndTickle:CreateBtns(frameName, parent, btnText, emote)
     local points = {}
-    if (btnText == "Tickle") then points[1] = 100 points[2] = -100 else points[1] = 0 points[2] = -20  end
+    if (btnText == "Tickle") then points[1] = 100 points[2] = -100 else points[1] = 0 points[2] = -40  end
     ZUI_GUI.frame = CreateFrame("Button", frameName, parent)
     frame = ZUI_GUI.frame
     frame:SetFrameStrata("HIGH")
     frame:SetFrameLevel(0)
     frame:SetSize(64, 20)
     frame:SetPoint("TOPLEFT", points[1], points[2])
-    frame:SetScript("OnClick", function() DoEmote(emote) end)
+    frame:SetScript("OnClick", function() ZUI_LickAndTickle:btnClicked(emote) end)
     
     text = frame:CreateFontString("text", "HIGH")
     text:SetPoint("CENTER")
@@ -67,4 +70,25 @@ function ZUI_LickAndTickle:CreateBtns(frameName, parent, btnText, emote)
     text:SetText(btnText)  
 end
 
--- track if target has been licked or tickled
+function ZUI_LickAndTickle:btnClicked(emote)
+    DoEmote(emote)
+    local target = GetUnitName("target")
+
+    if (emote == "tickle") then 
+        if (#self.db.profile.tickled == 0) then table.insert(self.db.profile.tickled, target) end
+        local isInDB = false;
+        for i, name in ipairs(self.db.profile.tickled) do
+            if (name == target) then isInDB = true end
+        end
+        if (isInDB == false) then table.insert(self.db.profile.tickled, target) end
+        
+    else
+        if (#self.db.profile.licked == 0) then table.insert(self.db.profile.licked, target) end
+        local isInDB = false;
+        for i, name in ipairs(self.db.profile.licked) do
+            if (name == target) then isInDB = true end
+        end
+        if (isInDB == false) then table.insert(self.db.profile.licked, target) end
+        
+    end
+end
